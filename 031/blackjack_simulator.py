e1342b1f9b129b793ae88161603d22295570a977
diplometrics <io.moneyball@gmail.com>
2019-02-08 18:28:30 -0500

    Significantly improved code
    - Added BJ payout (1.5)
    - Added numerous dealer checks to make it more accurate
     -- Checks for AA (22) bust and converts to 13
     -- Checks for bust while holding an ace and converts -10
     -- Checks for dealer BJ for immediate loss/push
     -- Checks for player BJ for immediate win/push
    - Improved the code efficiency by removing 0 from pop since it can be from the 'back' of the deck instead of 'front' (index)
    - Added calculations for edge
    - Offers more accurate overall play

--- 031/blackjack_simulator.py
+++ 031/blackjack_simulator.py
@@ -4,142 +4,177 @@ import math
 import time
 
 # configuration options
-simulations = 6000000
+simulations = 1000000
 num_decks = 4
 shuffle_perc = 75
 
+
 def simulate(queue, batch_size):
-	deck = []
+    deck = []
 
-	def new_deck():
-		std_deck = [
-		  # 2  3  4  5  6  7  8  9  10  J   Q   K   A
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-		]
+    def new_deck():
+        random.seed()
+        std_deck = [
+            # 2  3  4  5  6  7  8  9  10  J   Q   K   A
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+        ]
 
-		# add more decks
-		std_deck = std_deck * num_decks
+        # add more decks
+        std_deck = std_deck * num_decks
 
-		random.shuffle(std_deck)
+        random.shuffle(std_deck)
 
-		return std_deck[:]
+        return std_deck[:]
 
-	def play_hand():
-		dealer_cards = []
-		player_cards = []
+    def play_hand():
+        dealer_cards = []
+        player_cards = []
 
-		# deal initial cards
-		player_cards.append(deck.pop(0))
-		dealer_cards.append(deck.pop(0))
-		player_cards.append(deck.pop(0))
-		dealer_cards.append(deck.pop(0))
+        # deal initial cards
+        player_cards.append(deck.pop())
+        dealer_cards.append(deck.pop())
+        player_cards.append(deck.pop())
+        dealer_cards.append(deck.pop())
 
-		# deal player to 12 or higher
-		while sum(player_cards) < 12:
-			player_cards.append(deck.pop(0))
+        # TODO: Handle if player or dealer gets two aces, logic currently
+        #           has the hand as busting with a 22 (11+11)
+        if sum(dealer_cards) == 22 and len(dealer_cards) == 2: # AA hand
+            if dealer_cards[0] == 11:
+                dealer_cards[0] = 1
+        if sum(player_cards) == 22 and len(player_cards) == 2: # AA hand
+            if player_cards[0] == 11:
+                player_cards[0] = 1
 
-		# deal dealer on soft 17
-		while sum(dealer_cards) < 18:
-			exit = False
-			# check for soft 17
-			if sum(dealer_cards) == 17:
-				exit = True
-				# check for an ace and convert to 1 if found
-				for i, card in enumerate(dealer_cards):
-					if card == 11:
-						exit = False
-						dealer_cards[i] = 1
+        # deal player to 12 or higher
+        while sum(player_cards) < 12:
+            player_cards.append(deck.pop())
 
-			if exit:
-				break
+       
+        # deal dealer on soft 17
+        while sum(dealer_cards) < 18:
+            hand_value = sum(dealer_cards)
+            exit = False
+            # check for soft 17
+            if sum(dealer_cards) == 17:
+                exit = True
 
-			dealer_cards.append(deck.pop(0))
+            #deal new card
+            dealer_cards.append(deck.pop())
+            new_card_value = sum(dealer_cards) - hand_value
 
-		p_sum = sum(player_cards)
-		d_sum = sum(dealer_cards)
+            #check if ace and if bust, then adjust ace value
+            if sum(dealer_cards) > 21 and new_card_value == 11:
+                for i, card in enumerate(dealer_cards):
+                    if card == 11:
+                        exit = False
+                        dealer_cards[i] = 1
+            if exit:
+                break
 
-		# dealer bust
-		if d_sum > 21:
-			return 1;
-		# dealer tie
-		if d_sum == p_sum:
-			return 0;
-		# dealer win
-		if d_sum > p_sum:
-			return -1;
-		# dealer lose
-		if d_sum < p_sum:
-			return 1
+        p_sum = sum(player_cards)
+        d_sum = sum(dealer_cards)
 
-	# starting deck
-	deck = new_deck()
+        # check for dealer BJ
+        if d_sum == 21 and len(dealer_cards) == 2:
+            if p_sum == 21 and len(player_cards) == 2: 
+                return 0; # dealer and player has bj - push
+            else:
+                return -1; # dealer has blackjack
+        
+        # check for player BJ
+        if p_sum == 21 and len(player_cards) == 2:
+            if p_sum == 21 and len(dealer_cards) == 2: 
+                return 0; # dealer and player has bj - push
+            else:
+                return 1.5; # player has blackjack
 
-	# play hands
-	win = 0
-	draw = 0
-	lose = 0
-	for i in range(0, batch_size):
-		# reshuffle cards at shuffle_perc percentage
-		if (float(len(deck)) / (52 * num_decks)) * 100 < shuffle_perc:
-			deck = new_deck()
+        # dealer bust
+        if d_sum > 21:
+            return 1;
+        # dealer tie
+        if d_sum == p_sum:
+            return 0;
+        # dealer win
+        if d_sum > p_sum:
+            return -1;
+        # dealer lose
+        if d_sum < p_sum:
+            return 1;
 
-		# play hand
-		result = play_hand()
+    # starting deck
+    deck = new_deck()
 
-		# tally results
-		if result == 1:
-			win += 1
-		if result == 0:
-			draw += 1
-		if result == -1:
-			lose += 1
+    # play hands
+    win = 0
+    draw = 0
+    lose = 0
+    for i in range(0, batch_size):
+        # reshuffle cards at shuffle_perc percentage
+        if (float(len(deck)) / (52 * num_decks)) * 100 < shuffle_perc:
+            deck = new_deck()
 
-	# add everything to the final results
-	queue.put([win, draw, lose])
+        # play hand
+        result = play_hand()
+
+        # tally results
+        if result == 1.5: #blackjack!
+            win += 1.5
+        if result == 1:
+            win += 1
+        if result == 0:
+            draw += 1
+        if result == -1:
+            lose += 1
+
+    # add everything to the final results
+    queue.put([win, draw, lose])
 
 
-start_time = time.time()
+if __name__ == '__main__':
+    start_time = time.time()
 
-# simulate
-cpus = multiprocessing.cpu_count()
-batch_size = int(math.ceil(simulations / float(cpus)))
+    # simulate
+    cpus = multiprocessing.cpu_count()
+    batch_size = int(math.ceil(simulations / float(cpus)))
 
-queue = multiprocessing.Queue()
+    queue = multiprocessing.Queue()
 
-# create n processes
-processes = []
+    # create n processes
+    processes = []
 
-for i in range(0, cpus):
-	process = multiprocessing.Process(target=simulate, args=(queue, batch_size))
-	processes.append(process)
-	process.start()
+    for i in range(0, cpus):
+        process = multiprocessing.Process(target=simulate, args=(queue, batch_size))
+        processes.append(process)
+        process.start()
 
-# wait for everything to finish
-for proc in processes:
-	proc.join()
+    # wait for everything to finish
+    for proc in processes:
+        proc.join()
 
-finish_time = time.time() - start_time
+    finish_time = time.time() - start_time
 
-# get totals
-win = 0
-draw = 0
-lose = 0
+    # get totals
+    win = 0
+    draw = 0
+    lose = 0
 
-for i in range(0, cpus):
-	results = queue.get()
-	win += results[0]
-	draw += results[1]
-	lose += results[2]
+    for i in range(0, cpus):
+        results = queue.get()
+        win += results[0]
+        draw += results[1]
+        lose += results[2]
 
-print
-print ('  cores used: %d' % cpus)
-print ('  total simulations: %d' % simulations)
-print ('  simulations/s: %d' % (float(simulations) / finish_time))
-print ('  execution time: %.2fs' % finish_time)
-print ('  win percentage: %.2f%%'  % ((win / float(simulations)) * 100))
-print ('  draw percentage: %.2f%%' % ((draw / float(simulations)) * 100))
-print ('  lose percentage: %.2f%%' % ((lose / float(simulations)) * 100))
-print
+    print("Win", win)
+    print("Draw", draw)
+    print("Lose", lose)
+    print('  cores used: %d' % cpus)
+    print('  total simulations: %d' % simulations)
+    print('  simulations/s: %d' % (float(simulations) / finish_time))
+    print('  execution time: %.2fs' % finish_time)
+    print('  win percentage: %.2f%%' % ((win / float(simulations)) * 100))
+    print('  draw percentage: %.2f%%' % ((draw / float(simulations)) * 100))
+    print('  lose percentage: %.2f%%' % ((lose / float(simulations)) * 100))
+    print("Overall winnings: {} (edge = {} %)".format("{0:.2f}".format(win-lose), "{0:.3f}".format(100.0 * (win-lose) / simulations )))
